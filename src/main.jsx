4e2dbbae6544885230db312291790a30b8f974d8
gomezba <joel_gomezba@hotmail.com>
2024-09-18 20:24:18 -0600

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+
+const img = <img src='/assets/react.svg' />
+
+root.render(img)
